d6dd87b2213d295b078656063309542428f878ca
Zhao Yuhang <2546789017@qq.com>
2024-02-06 20:05:29 +0800

    update

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-dotnet build ProxyTool.sln --configuration Release --no-incremental --nologo --verbosity normal
+::dotnet build ProxyTool.sln --configuration Release --no-incremental --nologo --verbosity normal
+dotnet publish ProxyTool.sln --configuration Release --no-self-contained --nologo --verbosity normal
 endlocal
 pause
 exit /b 0
